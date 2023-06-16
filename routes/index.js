ecfe2ae934af916e8577619537ba4bc497cf8254
Valeriy Nikonovich <nikonovi4v@yandex.ru>
2023-06-17 00:11:05 +0300

    v2

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ const router = require("express").Router();
 const userRoutes = require("./users");
 const cardRouter = require("./cards");
 
-router.get("/", (req, res) => {
-  res.send("Hello word3");
-});
-
 router.use("/users", userRoutes);
 router.use("/cards", cardRouter);
+router.use('*', (req, res) => {
+  res.status(404).send({
+    message: 'Данная страница не существует'
+  })
+})
 
 module.exports = router;
