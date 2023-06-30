6e273bae209383da5932873f27c6420e5bb9b12d
Valeriy Nikonovich <nikonovi4v@yandex.ru>
2023-06-30 22:08:26 +0300

    backand auth v2

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const auth = require("../middlewares/auth");
 
 
 router.use("/", authRouter);
-router.use(auth);
+// router.use(auth);
 router.use("/users", userRoutes);
 router.use("/cards", cardRouter);
 router.use("*", (req, res) => {
