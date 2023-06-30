712100e2f2f6d61a1b275b21da97fbf2b0e1f19b
Valeriy Nikonovich <nikonovi4v@yandex.ru>
2023-06-30 21:29:13 +0300

    backand auth v2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,17 @@
 const jwt = require('jsonwebtoken')
+const UnauthorizedError = require("../errors/unauthorized-error")
 
 const auth = (req, res, next) => {
   const token = req.cookies.jwt;
   if (!token) {
-    res.status(401).send({ message: 'Необходима авторизация' });
+    return next(new UnauthorizedError('ошибка') )
   }
   let payload;
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
+    next(new UnauthorizedError('ошибка') )
+    // return res.status(401).send({ message: 'Необходима авторизация' });
   }
   req.user = payload;
   next();
