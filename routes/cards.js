cc90bbd8ca42abb037ec17264badb166bdd9c6a7
Valeriy Nikonovich <nikonovi4v@yandex.ru>
2023-07-01 06:01:27 +0300

    backand auth v2

--- routes/cards.js
+++ routes/cards.js
@@ -14,7 +14,7 @@ router.get("/", getCards);
 router.post("/", celebrate({
   body: Joi.object().keys({
   name: Joi.string().required().min(2).max(30),
-  link: Joi.string().required().min(2).max(30),
+  link: Joi.string().pattern(/https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/),
 }),
 }), createCard);
 
